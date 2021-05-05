a9ef57a9c2b7094bf8fcfe6653ea7f33fd5910f8
agustiandm <ikhwanagus01@gmail.com>
2021-05-05 09:55:35 +0700

    Initial Commit

---
+++ postview/postviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  postviewApp.swift
+//  postview
+//
+//  Created by Agustian DM on 05/05/21.
+//
+
+import SwiftUI
+
+@main
+struct postviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
