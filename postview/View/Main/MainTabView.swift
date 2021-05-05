6e0087670c63a6a9a5cfc82de3da1a5a1b6a1843
agustiandm <ikhwanagus01@gmail.com>
2021-05-05 10:17:04 +0700

    Setup File

---
+++ postview/View/Main/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  postview
+//
+//  Created by Agustian DM on 05/05/21.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
